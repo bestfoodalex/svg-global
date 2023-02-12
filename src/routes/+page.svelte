a033731fd71057d3805fb858011930497e4295df
Alex Jewell <al3xj3w3ll@gmail.com>
2023-02-11 19:13:43 -0600

    Add tailwind and establish svg groups

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Map from '../lib/map.svelte'
+</script>
+
+<div class="container mx-auto px-4">
+  <h1>SVG Global</h1>
+  <div class="flex">
+    <Map />
+  </div>
+</div>
