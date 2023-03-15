678c01e8183a9d21e72b023535f91b2feac3240f
Alex Jewell <al3xj3w3ll@gmail.com>
2023-03-15 13:33:18 -0500

    Support countries

--- src/components/ContinentData.svelte
+++ src/components/ContinentData.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
-  import { continentsData } from '../data'
-  import { continent } from '../stores'
+  import { continentsData, countriesData } from '../data'
+  import type { CountriesData } from '../types'
+  import { continent, country } from '../stores'
+  
+  $: $continent, watchSelections()
+  $: $country, watchSelections()
+
+  let filteredCountries = [] as CountriesData
+  let countryName = ''
+
+  const watchSelections = () => {
+    filteredCountries = countriesData.filter(c => c.continentKey === $continent)
+    countryName = countriesData.find(c => c.key === $country)?.name ?? `No Country Found for key ${$country}`
+  }
 </script>
 
 <div class="continent-data">
-  {#if $continent}
+  {#if $continent && $country === null}
     <h3>{continentsData[$continent]?.name || 'Continent Not Found'}</h3>
-    {#if continentsData[$continent]?.description}
-      <p>{continentsData[$continent]?.description}</p>
-    {/if}
+    <p>Pick a country below to see its Travel Guides!</p>
+    <ul>
+      {#each filteredCountries as cntry}
+        <li>
+          <a href={'#'} on:click|preventDefault={() => { $country = cntry.key }}>
+            {cntry.name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  {:else if $country}
+    <a href={'#'} on:click|preventDefault={() => { $country = null }}>
+      &lt; Back to {$continent ? continentsData[$continent]?.name : 'continent'}
+    </a>
+    <h3>{countryName} Travel Guides</h3>
   {/if}
 </div>
