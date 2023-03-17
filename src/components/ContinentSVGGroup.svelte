c09e02fd474c7fcbb88539326fa1ebc3d91453b8
Alex Jewell <al3xj3w3ll@gmail.com>
2023-03-17 10:18:34 -0500

    Reuse continent group code

---
+++ src/components/ContinentSVGGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { continent } from '../stores'
+  import type { Continents } from '../types'
+
+  export let continentName: Continents | null
+  const gClass = 'contspot hover:cursor-pointer transition ease-in-out delay-200 duration-150'
+</script>
+
+<g
+  class={$continent === continentName ? `${gClass} fill-primary-orange` : `${gClass} hover:fill-primary-orange`}
+  id={`continent-${continentName}`}
+  on:click={() => { $continent = continentName }}
+  on:keydown={() => { $continent = continentName }}
+>
+  <slot></slot>
+</g>
